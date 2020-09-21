ca73943585e9b8c8a9dec43ccf62ac637acdf96d
Kévin Cortacero <kevin.cortacero@inserm.fr>
2020-09-21 18:44:05 +0200

    new functions

--- src/cania/utils/image.py
+++ src/cania/utils/image.py
@@ -32,6 +32,10 @@ def write_rgb(filename, rgb_image):
     cv2.imwrite(filename, rgb_image)
 
 
+def write_bgr(filename, rgb_image):
+    cv2.imwrite(filename, rgb2bgr(rgb_image))
+
+
 def write_tiff():
     pass
 
@@ -54,6 +58,10 @@ def bgr2hsv(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 
+def rgb2bgr(img):
+    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
 """ channels """
 
 
@@ -68,6 +76,7 @@ def overlay(img, mask, color=[255, 255, 0], alpha=0.4):
     # Ref: http://www.pyimagesearch.com/2016/03/07/transparent-overlays-with-opencv/
     out = img.copy()
     img_layer = img.copy()
+    print(mask.shape)
     img_layer[np.where(mask)] = color
     overlayed = cv2.addWeighted(img_layer, alpha, out, 1 - alpha, 0, out)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -79,3 +88,30 @@ def fill_ellipses(mask, ellipses):
     for ellipse in ellipses:
         cv2.ellipse(mask, ellipse, 1, thickness=-1)
     return mask
+
+
+""" operations """
+
+def resize(img, scale):
+    return cv2.resize(img, scale)
+
+
+def imfill(img):
+    # https://www.learnopencv.com/filling-holes-in-an-image-using-opencv-python-c/
+    im_floodfill = img.copy()
+
+    # Mask used to flood filling.
+    # Notice the size needs to be 2 pixels than the image.
+    h, w = img.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+
+    # Floodfill from point (0, 0)
+    cv2.floodFill(im_floodfill, mask, (0,0), 255)
+
+    # Invert floodfilled image
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+    # Combine the two images to get the foreground.
+    im_out = img | im_floodfill_inv
+
+    return im_out
